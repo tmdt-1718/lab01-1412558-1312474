38f1f826679710ce48136565125d467eb701dae3
Phạm Quốc Toàn <bossdiemmaimai@gmail.com>
2017-10-05 20:38:39 +0700

    comment for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'posts', to:'posts#index'
-resources :posts
+resources :posts do
+  resources :comments
+end
+
 root 'home#index'
 get '/about', to: 'about#index'
 get '/home', to: 'home#index'
