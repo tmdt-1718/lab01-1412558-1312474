851dd5b704dea2860ccd158298be3d67974902a7
Phạm Quốc Toàn <bossdiemmaimai@gmail.com>
2017-10-06 20:49:56 +0700

    create blog

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'posts', to:'posts#index'
+resources :posts do
+  resources :comments
+end
+
+root 'home#index'
+get '/about', to: 'about#index'
+get '/home', to: 'home#index'
+get '/posts', to: 'posts#index'
+get '/albums', to: 'albums#index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
